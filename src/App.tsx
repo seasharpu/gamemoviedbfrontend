cb42ad177d7c86e00b66b77f1c43431d106ff230
Alexander <56878276+seasharpu@users.noreply.github.com>
2024-04-17 22:23:38 +0200

    Revert "Navbar"

--- src/App.tsx
+++ src/App.tsx
@@ -2,33 +2,26 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Searchbar } from './components/molecules/Searchbar';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import NavBar from "./components/molecules/Navbar";
-import Home from "./pages/Home";
-import User from "./pages/User";
-import Game from "./pages/Game";
-import Franchise from "./pages/Franchise";
-import Fourohfour from "./pages/Fourohfour";
 
 function App() {
   return (
     <div className="App">
-   
-  
-  
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<NavBar />}>
-          <Route index element={<Home />} />
-          <Route path="user" element={<User />} />
-          <Route path="game" element={<Game />} />
-          <Route path="franchise" element={<Franchise />} />
-          <Route path="*" element={<Fourohfour />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-    <Searchbar placeholder="hej" styling=""></Searchbar>
-  </div>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <Searchbar placeholder="hej" styling=""></Searchbar>
+      </header>
+    </div>
   );
 }
 
--- src/pages/fourohfour.tsx
+++
@@ -1,5 +0,0 @@
-export default function Fourohfour() {
-  return (
-    <p>Fourohfour</p>
-  );
-}
