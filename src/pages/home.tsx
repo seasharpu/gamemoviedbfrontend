07d732a375a8e0963ec6a17775e124fa1f3dac48
MatNimv <matilda.nimvik@gmail.com>
2024-04-25 15:45:14 +0200

    home page components added

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,5 +1,22 @@
+import { Searchbar } from "../components/molecules/Searchbar";
+import Title from "../components/atoms/Title";
+import { BodyText } from "../components/atoms/BodyText";
+import { Song } from "../components/molecules/Song";
+
 export default function Home() {
+
+
   return (
-    <p>This is the Home Page</p>
+    <div className="bg-dark-purple h-screen w-full flex flex-col items-center">
+      <Searchbar placeholder="games, soundtracks..." styling="w-4/5 mt-8" type=""></Searchbar>
+      <div className="w-4/5 mt-20">
+        <Title text="TITLE OF WEBPAGE" text_color="white" styling="" size="h2"></Title>
+        <BodyText text="Description of webpage something." text_color="white" styling="" size="medium"></BodyText>
+      </div>
+      <div className="w-4/5 mt-20">
+        <Title text="Listen to this!" text_color="white" styling="mb-3" size="h4"></Title>
+        <Song title="Victory" composer="Joris De Man" thumbnail={undefined}></Song>
+      </div>
+    </div>
   );
 }
