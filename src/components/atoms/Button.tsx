0706944b9c4ea1e76d6fc3966cac6e4867cb75e8
Alexander <56878276+seasharpu@users.noreply.github.com>
2024-04-25 16:57:42 +0200

    added styling to User (not logged in page) and changed structure of Button component

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -1,7 +1,18 @@
-export const Button = (textInput: string, textSize: string, textColor: string, boxWidth: string, boxHeight: string, boxColor: string) => {
+import React from 'react';
 
+const Button = ({ textInput, textSize, textColor, boxWidth, boxHeight, boxColor }: {
+    textInput: string;
+    textSize: string;
+    textColor: string;
+    boxWidth: string;
+    boxHeight: string;
+    boxColor: string;
+}) => {
     return (
-        <p className={`bg-${boxColor} text-${textColor} text-[${textSize} text-center w-[${boxWidth}] h-[${boxHeight}}`}>{textInput}</p>
+        <div className={`${boxColor} flex items-center justify-center ${boxWidth} ${boxHeight}`}>
+            <p className={`${textColor} ${textSize} `}>{textInput}</p>
+        </div>
+
     );
 }
 
--- src/pages/user.tsx
+++ src/pages/user.tsx
@@ -1,5 +1,22 @@
+import React from 'react';
+import { BodyText } from '../components/atoms/BodyText';
+import Button from '../components/atoms/Button';
+import { Searchbar } from '../components/molecules/Searchbar';
+
 export default function User() {
+  let loggedIn: boolean = false;
+
   return (
-    <p>This is the User Page</p>
+    <div className="bg-dark-purple h-screen w-full flex flex-col items-center">
+      <Searchbar placeholder="games, soundtracks..." styling="w-4/5 mt-8" type=""></Searchbar>
+      <div className="w-4/5 mt-20 flex flex-col items-center">
+        <BodyText text="Get recommendations and add favourite soundtrack and games with an account!" text_color="white" styling="" size="medium" />
+        <Button textInput="Sign in" textSize="text-sm" textColor="text-green" boxWidth="w-20" boxHeight="h-10" boxColor="bg-main-purple" />
+        <BodyText text="Or" text_color="white" styling="" size="medium" />
+        <Button textInput="Register" textSize="text-sm" textColor="text-green" boxWidth="w-20" boxHeight="h-10" boxColor="bg-main-purple" />
+      </div>
+    </div>
   );
 }
+
+
