6897203d35dd450002618a7c13415a7b306b4af2
MatNimv <matilda.nimvik@gmail.com>
2024-05-07 16:48:10 +0200

    hover and cursor

--- src/components/molecules/Navbar.jsx
+++ src/components/molecules/Navbar.jsx
@@ -2,50 +2,47 @@ import { Outlet, Link, useLocation } from "react-router-dom";
 import { BiSolidHomeAlt2 } from "react-icons/bi";
 import { IoLibrary } from "react-icons/io5";
 import { FaUser } from "react-icons/fa6";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 const NavBar = () => {
   const [isActive, setIsActive] = useState("/")
 
   //svg containers
-  const con_base_styling = "w-16 h-16 flex justify-center items-end rounded-full relative overflow-hidden text-clip transition-all"
-  const con_non_active_styling = "bg-green"
-  const con_active_styling = "bg-main-purple"
+  //inner-border-2 inner-border-blue-purple border-blue-purple border-2
+  const con_base_styling = "w-16 h-16 flex justify-center items-end rounded-full relative transition-colors z-10 hover:bg-main-purple"
+  const con_non_active_styling = "bg-main-purple hover:bg-green"
+  const con_active_styling = "bg-main-purple overflow-hidden "
 
   //svgs
-  const base_styling = "w-14 h-14 absolute -bottom-1.5 z-10 transition-all"
+  const base_styling = "w-14 h-14 absolute -bottom-1.5 z-20 transition-colors"
   const non_active_styling = `text-blue-purple`
   const active_styling = 'text-green'
 
 
   const change_nav = (location) => {
     setIsActive(location)
-    console.log(location);
   }
 
-  useEffect(() => {
-
-  }, [isActive])
 
   return (
     <>
       <nav>
         <ul className="flex col justify-around bg-blue-purple h-28 items-center">
-          <Link to="/" >
-            <li className={`${con_base_styling} ${isActive === "/" ? con_active_styling : con_non_active_styling}`} onClick={() => { change_nav("/") }}>
+          <Link to="/" className={isActive === "/" ? "cursor-auto" : "cursor-pointer"} onClick={() => { change_nav("/") }}>
+            <li className={`${con_base_styling} ${isActive === "/" ? con_active_styling : con_non_active_styling}`} >
               <BiSolidHomeAlt2 className={`${base_styling} ${isActive === "/" ? active_styling : non_active_styling}`} />
             </li>
           </Link>
 
-          <Link to="/franchise">
-            <li className={`${con_base_styling} ${isActive === "/franchise" ? con_active_styling : con_non_active_styling}`} onClick={() => { change_nav("/franchise") }} >
+          <Link to="/franchise" className={isActive === "/franchise" ? "cursor-auto" : "cursor-pointer"} onClick={() => { change_nav("/franchise") }} >
+            <li className={`${con_base_styling} ${isActive === "/franchise" ? con_active_styling : con_non_active_styling}`}>
               <IoLibrary className={`${base_styling} ${isActive === "/franchise" ? active_styling : non_active_styling}`} />
             </li>
           </Link>
 
-          <Link to="/user">
+          <Link to="/user" className={isActive === "/user" ? "cursor-auto" : "cursor-pointer"} onClick={() => { change_nav("/user") }}>
             <li className={`${con_base_styling} ${isActive === "/user" ? con_active_styling : con_non_active_styling}`}>
-              <FaUser className={`${base_styling} ${isActive === "/user" ? active_styling : non_active_styling}`} onClick={() => { change_nav("/user") }} />
+              <FaUser className={`${base_styling} ${isActive === "/user" ? active_styling : non_active_styling}`}  />
             </li>
           </Link>
         </ul>
