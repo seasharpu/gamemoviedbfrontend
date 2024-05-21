3ea092ff219d8bef6cb639cfed4217b9654dbea2
MatNimv <matilda.nimvik@gmail.com>
2024-05-21 13:49:12 +0200

    links put into map

--- src/components/molecules/Navbar.jsx
+++ src/components/molecules/Navbar.jsx
@@ -2,10 +2,10 @@ import { Outlet, Link, useLocation } from "react-router-dom";
 import { BiSolidHomeAlt2 } from "react-icons/bi";
 import { IoLibrary } from "react-icons/io5";
 import { FaUser } from "react-icons/fa6";
-import { useState } from "react";
+import React, { useState } from "react";
 
 const NavBar = () => {
-  const [isActive, setIsActive] = useState("/")
+  const [isActive, setIsActive] = useState(window.location.path)
 
   //svg containers
   const con_base_styling = "w-16 h-16 flex justify-center items-end rounded-full relative transition-colors z-10 hover:bg-main-purple"
@@ -22,28 +22,40 @@ const NavBar = () => {
     setIsActive(location)
   }
 
+  const nav_items = [
+    {
+      "link": "/",
+      "icon": <BiSolidHomeAlt2></BiSolidHomeAlt2>
+    },
+    {
+      "link": "/franchise",
+      "icon": <IoLibrary></IoLibrary>
+    },
+    {
+      "link": "/user",
+      "icon": <FaUser></FaUser>
+    }
+  ]
+
 
   return (
     <>
       <nav>
         <ul className="flex col justify-around bg-blue-purple h-28 items-center ">
-          <Link to="/" className={isActive === "/" ? "cursor-auto" : "cursor-pointer"} onClick={() => { change_nav("/") }}>
-            <li className={`${con_base_styling} ${isActive === "/" ? con_active_styling : con_non_active_styling}`} >
-              <BiSolidHomeAlt2 className={`${base_styling} ${isActive === "/" ? active_styling : non_active_styling}`} />
-            </li>
-          </Link>
-
-          <Link to="/franchise" className={isActive === "/franchise" ? "cursor-auto" : "cursor-pointer"} onClick={() => { change_nav("/franchise") }} >
-            <li className={`${con_base_styling} ${isActive === "/franchise" ? con_active_styling : con_non_active_styling}`}>
-              <IoLibrary className={`${base_styling} ${isActive === "/franchise" ? active_styling : non_active_styling}`} />
-            </li>
-          </Link>
-
-          <Link to="/user" className={isActive === "/user" ? "cursor-auto" : "cursor-pointer"} onClick={() => { change_nav("/user") }}>
-            <li className={`${con_base_styling} ${isActive === "/user" ? con_active_styling : con_non_active_styling}`}>
-              <FaUser className={`${base_styling} ${isActive === "/user" ? active_styling : non_active_styling}`}  />
-            </li>
-          </Link>
+          {nav_items.map((item, index) => (
+                <Link 
+                  key={index} 
+                  to={item.link} 
+                  className={isActive === item.link ? "cursor-auto" : "cursor-pointer"} 
+                  onClick={() => { change_nav(item.link) }}
+                  >
+                  <li className={`${con_base_styling} ${isActive === item.link ? con_active_styling : con_non_active_styling}`} >
+                    {React.cloneElement(item.icon, {
+                        className: `${base_styling} ${isActive === item.link ? active_styling : non_active_styling}`,
+                    })}
+                  </li>
+                </Link>
+          ))}
         </ul>
       </nav>
       <Outlet />
