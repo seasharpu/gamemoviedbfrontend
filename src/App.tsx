08683282defe3f3ae615b91d5177e6573ec5ffc7
MatNimv <matilda.nimvik@gmail.com>
2024-06-10 15:32:44 +0200

    namechange Home

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import './App.css';
 import { Searchbar } from './components/molecules/Searchbar';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from "./components/molecules/Navbar";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import User from "./pages/User";
 import Game from "./pages/Game";
 import Franchise from "./pages/Franchise";
